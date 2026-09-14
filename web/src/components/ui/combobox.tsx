9a377b92387aaadbb73a96ce44de491d8cb7b0ec
RedwindA <128586631+RedwindA@users.noreply.github.com>
2026-09-14 13:18:56 +0800

    fix(web): prevent combobox dropdowns opening on dialog autofocus (#7365)
    * fix(web): prevent combobox dropdowns opening on dialog autofocus
    
    * test(web): open version selector before typing in race regression
    
    (cherry picked from commit 2509e25fa0601d26f5b8fbb3194ec76689a91a77)

--- web/src/components/ui/combobox.tsx
+++ web/src/components/ui/combobox.tsx
@@ -113,9 +113,9 @@ function OptionCombobox(props: LegacyComboboxProps) {
       onInputValueChange={(value, details) => {
         if (details.reason === 'input-change') setSearch(value)
       }}
-      onOpenChange={(nextOpen) => {
+      onOpenChange={(nextOpen, details) => {
         setOpen(nextOpen)
-        setSearch('')
+        if (details.reason !== 'input-change') setSearch('')
       }}
       onValueChange={(option) => {
         if (option) props.onValueChange?.(option.value)
@@ -137,7 +137,8 @@ function OptionCombobox(props: LegacyComboboxProps) {
           onBlur={props.onBlur}
           onKeyDown={props.onKeyDown}
           onFocus={() => {
-            if (props.openOnFocus !== false) setOpen(true)
+            // Dialog autofocus should not expand a select-style combobox.
+            if (props.openOnFocus) setOpen(true)
           }}
           aria-label={props['aria-label']}
           aria-labelledby={props['aria-labelledby']}
